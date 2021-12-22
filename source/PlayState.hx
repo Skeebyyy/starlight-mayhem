335e3c9b6d9d518fc381d92de817f4db1554fe55
Skeebyyy <95153655+Skeebyyy@users.noreply.github.com>
2021-12-22 20:30:27 -0300

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1583,9 +1583,9 @@ class PlayState extends MusicBeatState
 				rgb(1);
 				rgb(0);
 			}
-		super.create(#if mobileC
+		super.create(); #if mobileC
         addVirtualPad(FULL, A_B);
-        #end);
+        #end
 	}
 
 	function checkanims() {
