42823b58760b02e14d2b65facb600adcfd734b2e
Skeebyyy <95153655+Skeebyyy@users.noreply.github.com>
2021-12-23 10:00:42 -0300

    Add files via upload

--- source/Controls.hx
+++ source/Controls.hx
@@ -513,11 +513,8 @@ class Controls extends FlxActionSet
 			case A_B_X_Y:
 				inline forEachBound(Control.ACCEPT, (action, state) -> addbuttonuUI(action, virtualPad.buttonA, state));
 				inline forEachBound(Control.BACK, (action, state) -> addbuttonuUI(action, virtualPad.buttonB, state));
-			case D:
-				
 			case NONE:
 		}
-	}
 	
 	public function setVirtualPadUi(virtualPad:FlxVirtualPad, ?DPad:FlxDPadMode, ?Action:FlxActionMode) {
 		if (DPad == null)
